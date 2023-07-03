fc9c67360c145066130a5083a88e7aae2e2b757b
Yuruna01 <yusupss1007@gmail.com>
2023-07-03 14:46:47 +0700

    Tambah Fitur Admin

--- application/views/admin/templates/admin-footer.php
+++ application/views/admin/templates/admin-footer.php
@@ -59,6 +59,16 @@ $(document).ready(function() {
     })
   })
 </script> -->
+<script>
+  function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+</script>
 
 </body>
 </html>
